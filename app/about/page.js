d538d0c82852c75947df5ee36d3bdea17887e4f4
Khush-Laptop <kapvm4444@gmail.com>
2026-01-20 14:24:51 +0530

    dynamic cabins count (33 completed)

--- app/about/page.js
+++ app/about/page.js
@@ -2,12 +2,18 @@ import Image from "next/image";
 import Link from "next/link";
 import Image1 from "@/public/about-1.jpg";
 import Image2 from "@/public/about-2.jpg";
+import { getCabins } from "@/app/_lib/data-service";
 
 export const metadata = {
   title: "About",
 };
 
-export default function Page() {
+export const revalidate = 86400;
+
+export default async function Page() {
+  const cabins = await getCabins();
+  const totalCabins = cabins.length;
+
   return (
     <>
       <div className="grid grid-cols-5 gap-x-24 gap-y-32 text-lg items-center">
@@ -25,10 +31,11 @@ export default function Page() {
               with nature and enjoying simple pleasures with family.
             </p>
             <p>
-              Our 8 luxury cabins provide a cozy base, but the real freedom and
-              peace you&apos;ll find in the surrounding mountains. Wander
-              through lush forests, breathe in the fresh air, and watch the
-              stars twinkle above from the warmth of a campfire or your hot tub.
+              Our {totalCabins} luxury cabins provide a cozy base, but the real
+              freedom and peace you&apos;ll find in the surrounding mountains.
+              Wander through lush forests, breathe in the fresh air, and watch
+              the stars twinkle above from the warmth of a campfire or your hot
+              tub.
             </p>
             <p>
               This is where memorable moments are made, surrounded by
